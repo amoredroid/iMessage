37c03e3c1657ac21e2f1d9e064cbb7693227822e
amoredroid <maleahamorgenson@gmail.com>
2017-07-22 23:05:19 +0800

    My Revised Work

---
+++ AndroidStudioProjects/Easy_Calc/app/src/main/java/amor/genson/com/easy_calc/Operations.java
@@ -0,0 +1,22 @@
+package amor.genson.com.easy_calc;
+
+/**
+ * Created by Genson on 20/07/2017.
+ */
+public class Operations {
+    public double actAdd(double firstNum, double secondNum) {
+        return firstNum + secondNum;
+    }
+
+    public double actSub(double firstNum, double secondNum) {
+        return firstNum - secondNum;
+    }
+
+    public double actMult(double firstNum, double secondNum) {
+        return firstNum * secondNum;
+    }
+
+    public double actDiv(double firstNum, double secondNum) {
+        return firstNum / secondNum;
+    }
+}
