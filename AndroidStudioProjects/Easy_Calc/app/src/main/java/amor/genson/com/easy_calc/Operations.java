d47585a099fa58d7d2a23e0ac94f8a4d014d5933
amoredroid <maleahamorgenson@gmail.com>
2017-07-30 07:55:24 +0800

    commit

--- AndroidStudioProjects/Easy_Calc/app/src/main/java/amor/genson/com/easy_calc/Operations.java
+++ AndroidStudioProjects/Easy_Calc/app/src/main/java/amor/genson/com/easy_calc/Operations.java
@@ -3,20 +3,20 @@ package amor.genson.com.easy_calc;
 /**
  * Created by Genson on 20/07/2017.
  */
-public class Operations {
-    public double actAdd(double firstNum, double secondNum) {
+class Operations {
+    double actAdd(double firstNum, double secondNum) {
         return firstNum + secondNum;
     }
 
-    public double actSub(double firstNum, double secondNum) {
+    double actSub(double firstNum, double secondNum) {
         return firstNum - secondNum;
     }
 
-    public double actMult(double firstNum, double secondNum) {
+    double actMult(double firstNum, double secondNum) {
         return firstNum * secondNum;
     }
 
-    public double actDiv(double firstNum, double secondNum) {
+    double actDiv(double firstNum, double secondNum) {
         return firstNum / secondNum;
     }
 }
